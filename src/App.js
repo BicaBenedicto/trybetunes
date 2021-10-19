efa360ebddd57e31f3e521ca5945773aeb7b5aa2
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-10-19 15:35:29 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Search from './components/Search';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import NotFound from './components/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <header>
+          <h1>
+            TrybeTunes
+          </h1>
+        </header>
+        <BrowserRouter>
+          <Route exact path="/" component={ Home } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" render={ () => (<Album />) } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/" component={ NotFound } />
+        </BrowserRouter>
+      </>
+    );
   }
 }
 
---
+++ src/components/ProfileEdit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        Login
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
