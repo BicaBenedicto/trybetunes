9ec999481e4d17ea521ef738a0dbfc63eebe4147
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-10-19 19:01:04 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -12,29 +12,29 @@ import Header from './components/Header';
 class App extends React.Component {
   render() {
     return (
-      <>
-        <header>
-          <h1>
-            TrybeTunes
-          </h1>
-        </header>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ Home } />
-            <Route path="/search" component={ Header } />
-            <Route path="/search" component={ Search } />
-            <Route path="/Album" component={ Header } />
-            <Route path="/album/:id" render={ () => (<Album />) } />
-            <Route path="/favorites" component={ Header } />
-            <Route path="/favorites" component={ Favorites } />
-            <Route path="/profile" component={ Header } />
-            <Route exact path="/profile" component={ Profile } />
-            <Route path="/profile/edit" component={ Header } />
-            <Route path="/profile/edit" component={ ProfileEdit } />
-            <Route path="*" component={ NotFound } />
-          </Switch>
-        </BrowserRouter>
-      </>
+      <BrowserRouter>
+        <Route path="*">
+          <header>
+            <h1>
+              TrybeTunes
+            </h1>
+          </header>
+        </Route>
+        <Route path="/search" component={ Header } />
+        <Route path="/Album" component={ Header } />
+        <Route path="/favorites" component={ Header } />
+        <Route path="/profile" component={ Header } />
+        <Route path="/profile/edit" component={ Header } />
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" render={ () => (<Album />) } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: '',
+      buttonDisable: true,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange({ target }) {
+    const { value } = target;
+    this.setState({
+      search: value,
+      buttonDisable: (value.length < 2),
+    });
+  }
+
   render() {
+    const { search, buttonDisable } = this.state;
     return (
       <div data-testid="page-search">
-        Login
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          placeholder="Nome do Artista ou Album"
+          value={ search }
+          onChange={ this.onInputChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="submit"
+          disabled={ buttonDisable }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
