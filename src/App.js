e31eac530ee8b2e13f861b341967c85bf279cc9d
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-10-20 05:10:36 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route path="/search" component={ Search } />
-          <Route path="/album/:id" render={ (props) => (<Album { ...props } />) } />
+          <Route path="/album/:id" component={ (props) => (<Album { ...props } />) } />
           <Route path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
--- src/components/Album.js
+++ src/components/Album.js
@@ -1,13 +1,70 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      album: {},
+      musics: [],
+    };
+
+    this.recoverMusics = this.recoverMusics.bind(this);
+  }
+
+  componentDidMount() {
+    this.recoverMusics();
+  }
+
+  recoverMusics() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    getMusics(id).then((response) => (
+      this.setState({
+        album: response[0],
+        musics: response
+          .filter(({ previewUrl }) => previewUrl)
+          .map(({ previewUrl, trackName, trackId }) => (
+            { trackName, previewUrl, trackId }
+          )),
+      })
+    ));
+  }
+
   render() {
+    const { album, musics } = this.state;
     return (
-      <div data-testid="page-album">
-        Login
+      <div className="album" data-testid="page-album">
+        <div className="album-info">
+          <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+          <h3 data-testid="album-name">{album.collectionName}</h3>
+          <h4 data-testid="artist-name">{album.artistName}</h4>
+        </div>
+        <div className="album-musics">
+          {musics.map(({ trackName, previewUrl, trackId }) => (
+            <div key={ trackId }>
+              <span>{trackName}</span>
+              <audio data-testid="audio-component" src={ previewUrl } controls>
+                <track kind="captions" />
+                O seu navegador não suporta o elemento
+                <code>audio</code>
+                .
+              </audio>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape(PropTypes.string).isRequired,
+  }).isRequired,
+};
 
 export default Album;
